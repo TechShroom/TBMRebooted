63594cf6f2b855b6dc9254d16b7548b609537372
Kenzie Togami <ket1999@gmail.com>
2014-08-21 12:09:49 -0700

    More Boilerplate™

---
+++ src/com/techshroom/mods/tbm/TBMMod.java
@@ -0,0 +1,8 @@
+package com.techshroom.mods.tbm;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "TBMMod", useMetadata = true, guiFactory = "com.techshroom.mods.tbm.gui.GuiFactory")
+public final class TBMMod {
+
+}
